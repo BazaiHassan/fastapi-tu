395baf1a45b2a6af823f73415ea57ae0c2866a92
Hbazai <bazaee.hassan@gmail.com>
2025-08-08 11:30:49 +0330

    exercise 02

--- core/main.py
+++ core/main.py
@@ -1,83 +1,58 @@
-from fastapi import FastAPI, status, Body, Path, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, status, HTTPException
+from schemas import ExpenseSchema, ExpenseUpdateSchema, ExpenseCreateSchema
+from typing import List
+from uuid import UUID, uuid4
+
 
 app = FastAPI(
     title="Expense Tracker API",
     description="A simple API to track expenses",
-    version="1.0.0"
+    version="1.0.1"
 )
 
 # Database
 db = []
 
-# Routes for expenses
-@app.get("/expenses", status_code=status.HTTP_200_OK)
+
+# Routes
+@app.get("/expenses", response_model=List[ExpenseSchema], status_code=status.HTTP_200_OK)
 def get_all_expenses():
-    return JSONResponse(
-        content={"data": db},
-        status_code=status.HTTP_200_OK
-    )
+    return db
 
-@app.post("/expenses", status_code=status.HTTP_201_CREATED)
-def add_expense(description: str = Body(min_length=2), amount: int = Body()):
-    new_id = len(db) + 1
-    new_expense = {
-        "id": new_id,
-        "description": description,
-        "amount": amount
-    }
+@app.post("/expenses", response_model=ExpenseSchema, status_code=status.HTTP_201_CREATED)
+def add_expense(expense: ExpenseCreateSchema):
+    new_expense = ExpenseSchema(id=uuid4(), **expense.model_dump())
     db.append(new_expense)
-    
-    return JSONResponse(
-        content={"data": new_expense},
-        status_code=status.HTTP_201_CREATED
-    )
+    return new_expense
 
-@app.get("/expenses/{id}", status_code=status.HTTP_200_OK)
-def get_expense_by_id(id: int = Path(..., gt=0)):
+@app.get("/expenses/{expense_id}", response_model=ExpenseSchema)
+def get_expense_by_id(expense_id: UUID):
     for expense in db:
-        if expense["id"] == id:
-            return JSONResponse(
-                content={"data": expense},
-                status_code=status.HTTP_200_OK
-            )
-    
+        if expense.id == expense_id:
+            return expense
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Expense not found"
     )
 
-@app.put("/expenses/{id}", status_code=status.HTTP_200_OK)
-def update_expense(
-    id: int = Path(..., gt=0),
-    description: str = Body(min_length=2),
-    amount: int = Body()
-):
-    for expense in db:
-        if expense["id"] == id:
-            expense["description"] = description
-            expense["amount"] = amount
-            
-            return JSONResponse(
-                content={"data": expense},
-                status_code=status.HTTP_200_OK
-            )
-    
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Expense not found"
-    )
-
-@app.delete("/expenses/{id}", status_code=status.HTTP_200_OK)
-def delete_expense(id: int = Path(..., gt=0)):
+@app.put("/expenses/{expense_id}", response_model=ExpenseSchema)
+def update_expense(expense_id: UUID, expense_update: ExpenseUpdateSchema):
     for index, expense in enumerate(db):
-        if expense["id"] == id:
-            deleted_expense = db.pop(index)
-            return JSONResponse(
-                content={"data": deleted_expense},
-                status_code=status.HTTP_200_OK
-            )
-    
+        if expense.id == expense_id:
+            update_data = expense_update.model_dump(exclude_unset=True)
+            updated_expense = expense.copy(update=update_data)
+            db[index] = updated_expense
+            return updated_expense
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Expense not found"
+    )
+
+@app.delete("/expenses/{expense_id}", response_model=ExpenseSchema)
+def delete_expense(expense_id: UUID):
+    for index, expense in enumerate(db):
+        if expense.id == expense_id:
+            return db.pop(index)
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Expense not found"
